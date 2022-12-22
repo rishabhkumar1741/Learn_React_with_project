bfabf12315a8f3fbb93ede4600bf37b60057c4c5
rishabhkumar <1741rishabh@gmail.com>
2022-12-22 12:16:40 +0530

    use useState() with complex data structures

--- README.md
+++ README.md
@@ -5,16 +5,23 @@
 - Styling
 - Static Pages
 ###  Project 1
-![alt text](/src/Assets/Images/ss1.png)
+![alt text](src/Assets/Images/ss1.png)
 ### Project 2
-![alt text](/src/Assets/Images/ss2.png)
+![alt text](src/Assets/Images/ss2.png)
 
 # Data Driven React 
 ## What we'll learn
 - Props
 - Creating Components from an array
 ### Project 3
-![alt text](/src/Assets/Images/ss3.png)
+![alt text](src/Assets/Images/ss3.png)
+
+- how to use useState() 
+- use useState() with complex data structures
+![alt text](src/Assets/Images/ss4.png)
+- 
+- 
+
 
 
 
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,39 @@
+import './Home.css'
+import Navbar from '../../Components/Navbar/Navbar';
+import Hero from '../../Components/Hero/Hero';
+import NewCard from '../../Components/NewCard/NewCard';
+import data from '../../Assets/data';
+import person1 from '../../Assets/Images/project3_card_person7.jpg';
+import person2 from '../../Assets/Images/project3_card_person6.jpg';
+import person10 from '../../Assets/Images/project3_card_person10.jpg';
+import person3 from '../../Assets/Images/project3_card_person1.jpg';
+import person4 from '../../Assets/Images/project3_card_person2.jpg';
+import person5 from '../../Assets/Images/project3_card_person3.jpg';
+import person6 from '../../Assets/Images/project3_card_person4.jpg';
+import person7 from '../../Assets/Images/project3_card_person5.jpg';
+import person8 from '../../Assets/Images/project3_card_person8.jpg';
+import person9 from '../../Assets/Images/project3_card_person9.jpg';
+
+
+function Home(){
+    let imgs = [person1,person2,person3,person4,person5,person6,person7,person8,person9,person10];
+let  img_index = -1;
+let newdata = data.map((data)=>{
+  img_index++;
+  return <NewCard key={data.id} title={data.title} id={data.id} country={data.country} desc ={data.desc}  price={data.price} name={data.name} img={imgs[img_index]} />
+})
+
+  return (<>
+  <Navbar></Navbar>
+  <Hero></Hero>
+  <div className='my_container'>
+    {newdata}
+  </div>
+  
+  
+
+  </>
+  );
+}
+
+export default Home;
